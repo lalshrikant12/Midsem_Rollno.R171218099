d8bfd2d400579152b13cf3e397aacacc873507f6
lalshrikant12 <lalshrikant12gmail.com>
2019-03-18 12:51:39 +0530

    2nd try created class

--- sub/cprog.cpp
+++ sub/cprog.cpp
@@ -6,4 +6,26 @@ class calcu
 	{x=a,y=b
 		cout<<"enter a="<<a<<"enter b"<<b;
 		sum=a+b;
-		
+	}
+	void diff(int x,int y)
+	{x=a,y=b;
+		cout<<"enter a="<<a<<"enter b"<<b;
+		diff=a-b;
+		cout<<diff;
+	}
+	void multiply(int x,int y)
+	{x=a,y=b;
+		cout<<"enter a="<<a<<"enter b"<<b;
+
+		mul=a*b;
+		cout<<multiply;
+	}
+	void divide(int x,int y)
+	{x=a,y=b
+		 cout<<"enter a="<<a<<"enter b"<<b;
+
+		 divide=a/b;
+		 cout<<divide;
+	}
+}; //class created
+
